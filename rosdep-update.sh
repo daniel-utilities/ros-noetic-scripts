7d4bb285c7585942c5c503b7f477a20c7ac0cdac
Daniel Kennedy <78428703+danielk-98@users.noreply.github.com>
2022-06-07 17:58:58 -0400

    Update rosdep-update.sh

--- rosdep-update.sh
+++ rosdep-update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
+rosdep --rosdistro=$ROS_DISTRO update
+
 if [ -z ${ROS_OS_OVERRIDE+x} ]; then
     rosdep --rosdistro=$ROS_DISTRO --os=$ROS_OS_OVERRIDE update
-else
-    rosdep --rosdistro=$ROS_DISTRO update
 fi
